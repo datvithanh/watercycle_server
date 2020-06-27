b042e11b618df796aa3decf35d9db97c7ca167a0
datvithanh98 <datvithanh98@gmail.com>
2020-06-27 10:13:22 +0700

    update

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/dashboard')
+def dashboard():
+    data = {'today_ppm': int(np.random.randint(1000)),
+        'today_usage': int(np.random.randint(1000)),
+        'ppm_history': [int(tmp) for tmp in np.random.randint(1000, size=30)],
+        'usage_history': [int(tmp) for tmp in np.random.randint(1000, size=30)]}
+    return jsonify(data)
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
