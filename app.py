c613279bbc6cbe9782a351097d756afcaf5e2ca5
datvithanh98 <datvithanh98@gmail.com>
2020-06-27 10:17:00 +0700

    fix: update metrics

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ CORS(app)
 
 @app.route('/dashboard')
 def dashboard():
-    data = {'today_ppm': int(np.random.randint(1000)),
+    data = {'today_tds': int(np.random.randint(1000)),
         'today_usage': int(np.random.randint(1000)),
-        'ppm_history': [int(tmp) for tmp in np.random.randint(1000, size=30)],
+        'tds_history': [int(tmp) for tmp in np.random.randint(1000, size=30)],
         'usage_history': [int(tmp) for tmp in np.random.randint(1000, size=30)]}
     return jsonify(data)
-    
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
